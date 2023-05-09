543b6a86ffcb081319121effa06f1f4d9d6da728
Kibrom Hailu <92378090+keba-python@users.noreply.github.com>
2023-05-09 19:20:14 +0300

    added course-schedule.python

---
+++ course-schedule.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        inrequest = defaultdict(int)
+        arr = []
+        for a,b in prerequisites:
+            graph[b].append(a)
+            inrequest[a] += 1 
+        
+        queue = deque()
+        for k in range(numCourses):
+            if inrequest[k] == 0:
+                queue.append(k)
+                
+        
+        while queue:
+            node = queue.pop()
+            arr.append(node)
+
+            for elem in graph[node]:
+                inrequest[elem] -= 1
+
+                if inrequest[elem] == 0:
+                    queue.append(elem)
+                    
+
+               
+
+        if len(arr) == numCourses:
+            return True
+        return False
